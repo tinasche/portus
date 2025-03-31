9c4014952eae570fb7292030bf1b7c17919c5e80
tinasche <tinashec93@live.com>
2025-03-31 10:03:01 +0200

    chore: edit info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {AboutComponent} from './sections/about/about.component';
 import {ExperienceComponent} from './sections/experience/experience.component';
 import {ProjectsComponent} from './sections/projects/projects.component';
 import {EducationComponent} from './sections/education/education.component';
 import {SkillsComponent} from './sections/skills/skills.component';
 import {ContactComponent} from './sections/contact/contact.component';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,22 @@ import {ContactComponent} from './sections/contact/contact.component';
   templateUrl: './app.component.html',
   styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      AOS.refresh()
+    }, 500)
+  }
+
+  ngOnInit(): void {
+    AOS.init({
+      delay: 1500,
+      once: true,
+      duration: 3000,
+      easing: 'ease-in-out',
+      startEvent: 'scroll'
+    })
+  }
+
   title = 'portfolio';
 }
