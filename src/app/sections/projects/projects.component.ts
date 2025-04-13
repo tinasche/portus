085e9504ad25b9da3c28e945eb89f346d2873f1c
Tinashe Chitakunye <36472653+tinasche@users.noreply.github.com>
2025-04-13 16:43:43 +0200

    Update projects with stack context

--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -19,15 +19,14 @@ type ProjectInfo = {
   styles: ``
 })
 export class ProjectsComponent {
-
+  
   projects: ProjectInfo[] = [
-    {name: 'Task Tracker', description: 'CLI based tool to keep track of your tasks. ', image: '', stack: ['Java', 'Gradle', 'Apache Commons CLI', 'h2'], url: 'https://github.com/tinasche/tasker'},
-    {name: 'Workout Tracker', description: 'CLI based tool to keep track of your tasks. ', image: '', stack: ['Spring Boot', 'Angular'], url: 'https://github.com/tinasche/workit'},
-    {name: 'Blogging platform', description: 'CLI based tool to keep track of your tasks. ', image: '', stack: ['.NET Core', 'NextJS'], url: 'https://github.com/tinasche/blogformer'},
+    {name: 'Task Tracker', description: 'CLI based tool to keep track of your tasks. ', image: '', stack: ['Java', 'Gradle', 'JDBC', 'Apache Commons CLI', 'h2'], url: 'https://github.com/tinasche/tasker'},
+    {name: 'Workout Tracker', description: 'Keep track of your workout routines and goals with this minimal web app.', image: '', stack: ['Spring Boot', 'Kotlin', 'Angular'], url: 'https://github.com/tinasche/workit'},
+    {name: 'Blogging platform', description: 'Share your experiences with a minimal blogging app.', image: '', stack: ['.NET Core','Minimal API', 'NextJS'], url: 'https://github.com/tinasche/blogformer'},
     {name: 'Github user activity', description: 'Get information about a github user from your commandline. You can see the user\'s repositories, projects and latest activity on their profile.', image: '', stack: ['Java', 'Gradle'], url: 'https://github.com/tinasche/tasker'},
-    {name: 'Vegeland', description: 'Get the freshest fruits and vegetables with the click of a button on your phone and enjoy the convenience of speedy delivery service right by your door step.', image: '', stack: ['Java', 'Gradle'], url: 'https://github.com/tinasche/tasker'},
-    {name: 'Movie Reservation', description: 'SaaS to make reservations with live updates on', image: '', stack: ['Spring Boot', 'NextJS', 'Clerk'], url: 'https://github.com/tinasche/tasker'},
+    {name: 'Vegeland', description: 'Fresh fruits and vegetables delivered with the click of a button and enjoy the convenience of speedy delivery service right by your door step.', image: '', stack: ['Java', 'Gradle'], url: 'https://github.com/tinasche/tasker'},
+    {name: 'Movie Reservation', description: 'SaaS to make reservations with real-time updates on ticket availability and theatre showings.', image: '', stack: ['Spring Boot', 'NextJS', 'Clerk'], url: 'https://github.com/tinasche/tasker'},
   ];
-
-
+  
 }
