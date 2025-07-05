43a67b53a41499a4a12eb694728cc02a4471cced
Tinashe Chitakunye <36472653+tinasche@users.noreply.github.com>
2025-07-05 23:00:12 +0200

    Update projects.component.ts

--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -22,7 +22,7 @@ export class ProjectsComponent {
   
   projects: ProjectInfo[] = [
     {name: 'Task Tracker', description: 'CLI based tool to keep track of your tasks. ', image: '', stack: ['Java', 'Gradle', 'JDBC', 'Apache Commons CLI', 'h2'], url: 'https://github.com/tinasche/tasker'},
-    {name: 'connStringGen', description: 'A VS code extension for working with connectionstring in .NET', image: '', stack: ['TypeScript', 'NodeJS'], url: 'https://github.com/tinasche/connstringgen'},
+    {name: 'ConnStringGen', description: 'A VS code extension for working with connectionstring in .NET', image: '', stack: ['TypeScript', 'NodeJS'], url: 'https://github.com/tinasche/connstringgen'},
     {name: 'Blogging platform', description: 'Share your experiences with a minimal blogging app.', image: '', stack: ['.NET Core','Minimal API', 'NextJS'], url: 'https://github.com/tinasche/blogformer'},
     {name: 'Github user activity', description: 'Get information about a github user from your commandline. You can see the user\'s repositories, projects and latest activity on their profile.', image: '', stack: ['Java', 'Gradle'], url: 'https://github.com/tinasche/tasker'},
     {name: 'Vegeland', description: 'Fresh fruits and vegetables delivered with the click of a button and enjoy the convenience of speedy delivery service right by your door step.', image: '', stack: ['Java', 'Gradle'], url: 'https://github.com/tinasche/tasker'},
